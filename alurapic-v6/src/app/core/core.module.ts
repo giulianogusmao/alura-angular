9c06c2bf8494d3086e300d9104094f2f324a7521
Giuliano Gusmão <gusmaoggg@gmail.com>
2018-08-23 15:12:34 -0300

    criando menu que é visivel apenas quando o usuario estiver logado

--- alurapic-v6/src/app/core/core.module.ts
+++ alurapic-v6/src/app/core/core.module.ts
@@ -4,11 +4,13 @@ import { RouterModule } from '@angular/router';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { NotifyModule } from '../shared/components/notify/notify.module';
+import { LoadingModule } from '../shared/components/loading';
+import { MenuModule } from '../shared/components/menu';
+import { ShowIfLoggedModule } from '../shared/directives/show-if-logged';
 
 import { HeaderComponent } from './header/header/header.component';
 import { RequestInterceptor } from './auth/request.interceptor';
 import { FooterComponent } from './footer/footer.component';
-import { LoadingModule } from '../shared/components/loading';
 
 @NgModule({
   imports: [
@@ -16,6 +18,8 @@ import { LoadingModule } from '../shared/components/loading';
     RouterModule,
     NotifyModule,
     LoadingModule,
+    MenuModule,
+    ShowIfLoggedModule,
   ],
   declarations: [
     HeaderComponent,
--- alurapic-v6/src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
+++ alurapic-v6/src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
@@ -7,6 +7,8 @@ import { UserService } from "../../../core/user/user.service";
 })
 export class ShowIfLoggedDirective implements OnInit {
 
+  private currentDisplay: string;
+
   constructor(
     private element: ElementRef<any>,
     private renderer: Renderer,
@@ -14,8 +16,22 @@ export class ShowIfLoggedDirective implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    // esconde elemento caso o usuário não esteja logado
-    !this.userService.isLogged()
-      && this.renderer.setElementStyle(this.element.nativeElement, 'display', 'none');
+    this.refreshCurrentDisplay();
+
+    this.userService.getUser().subscribe(user => {
+      if (user) {
+        this.renderer.setElementStyle(this.element.nativeElement, 'display', this.currentDisplay);
+      } else {
+        this.refreshCurrentDisplay();
+        // esconde elemento caso o usuário não esteja logado
+        this.renderer.setElementStyle(this.element.nativeElement, 'display', 'none');
+      }
+    });
+
+  }
+
+  refreshCurrentDisplay() {
+    // captura o display na inicialização do componente
+    this.currentDisplay = getComputedStyle(this.element.nativeElement).display;
   }
 }
